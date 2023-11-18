b626c889f0ccfd199dddb32b10110fb2da809f24
ninigibradze <ninigibradze@gmail.com>
2023-11-18 15:35:04 +0400

    fixed heights for sections

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name="home" className="bg-[#0a192f] w-full h-screen">
+    <div name="home" className="bg-[#0a192f] w-full py-44">
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center ">
         <p className="text-[#fc5657]">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
           Nini Gibradze
